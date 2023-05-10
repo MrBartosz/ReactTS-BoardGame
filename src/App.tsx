5f150cdb4edc9669e34932474981d733196bca2b
Bartek Maksimowski <maksimowski97@gmail.com>
2023-05-10 23:32:29 +0200

    Fix timeout bug in the evaluate function

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,11 @@ import { useState } from 'react';
 
 const cardIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 cardIds.sort(() => 0.5 - Math.random())
-console.log(cardIds);
 
 function App() {
 
-  const [moves, setMoves] = useState<number>(0)
-  const [bestScore, setBestScore] = useState<number>(
+  const [moves, setMoves] = useState(0)
+  const [bestScore, setBestScore] = useState(
     parseInt(localStorage.getItem('bestScore') || '0') || Number.MAX_SAFE_INTEGER
   )
   const finishGameCallback = () => {
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -22,7 +22,7 @@ function Board(props: BoardProps) {
   const [openCards, setOpenCards] = useState<Array<number>>([]);
   const [clearedCards, setClearedCards] = useState<Array<number>>([]);
   const [shouldDisableAllCards, setShouldDisableAllCards] = useState<boolean>(false);
-  const timeout = useRef<NodeJS.Timeout>(setTimeout(()=>{}));
+
 
   const disable = () => {
     setShouldDisableAllCards(true);
@@ -40,6 +40,7 @@ function Board(props: BoardProps) {
   const evaluate = () => {
     const [first, second] = openCards;
     enable();
+
     // check if first card is equal second card
     if ((first % 6 + 1) === (second % 6 + 1)) {
       setClearedCards((prev) => [...prev, first, second]);
@@ -47,32 +48,35 @@ function Board(props: BoardProps) {
       return;
     }
     // flip the cards back after 500ms duration
-    timeout.current = setTimeout(() => {
+    setTimeout(() => {
       setOpenCards([]);
     }, 500);
   }
 
   const handleCardClick = (id: number) => {
     if (openCards.length === 1) {
+
       // in this case we have alredy selected one card
       // this means that we are finishing a move
       setOpenCards((prev) => [...prev, id]);
       props.setMoves((moves) => moves + 1)
       disable();
     } else {
+
       // in this case this is the first card we select
-      clearTimeout(timeout.current);
       setOpenCards([id]);
     }
   };
 
   useEffect(() => {
-    let timeout: NodeJS.Timeout = setTimeout(()=>{});
+    let timeout: NodeJS.Timeout | null = null;
     if (openCards.length === 2) {
       timeout = setTimeout(evaluate, 300);
     }
     return () => {
-      clearTimeout(timeout);
+      if (timeout) {
+        clearTimeout(timeout);
+      }
     };
   }, [openCards]);
 
